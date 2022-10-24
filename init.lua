b7a5f0a3a291434675fa4b39bb2653397b3bda4c
DanRoscigno <dan@roscigno.com>
2022-10-23 21:23:30 -0400

    switch back to spell

--- init.lua
+++ init.lua
@@ -57,6 +57,26 @@ vim.g.mapleader = " "
 vim.api.nvim_set_keymap("n", "<leader>fr", ":History<CR>", { noremap = true })
 -- Open files in same directory as current file
 vim.api.nvim_set_keymap("n", "<leader>ff", ":e %:h/<C-d>", { noremap = true })
+-- Start Grammarous check
+vim.api.nvim_set_keymap("n", "<leader>gc", ":GrammarousCheck<CR>", { noremap = true })
+-- Cancel Grammarous check
+vim.api.nvim_set_keymap("n", "<leader>gq", ":GrammarousReset<CR>", { noremap = true })
+vim.cmd([[
+let g:grammarous#show_first_error=1
+let g:grammarous#hooks = {}
+function! g:grammarous#hooks.on_check(errs) abort
+  nmap <buffer><C-n> <Plug>(grammarous-move-to-next-error)
+  nmap <buffer><C-p> <Plug>(grammarous-move-to-previous-error)
+  nmap <buffer><C-f> <Plug>(grammarous-fixit)
+  nmap <buffer><C-i> <Plug>(grammarous-remove-error) <Plug>(grammarous-move-to-next-error)
+endfunction
+function! g:grammarous#hooks.on_reset(errs) abort
+  nunmap <buffer><C-n>
+  nunmap <buffer><C-p>
+  nunmap <buffer><C-f>
+  nunmap <buffer><C-i>
+endfunction
+]])
 
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
