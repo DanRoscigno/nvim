5d0fddfa2ea498eeadc135f5b3b5979fec0af452
DanRoscigno <dan@roscigno.com>
2022-10-27 17:07:30 -0400

    remove

--- lua/packages.lua
+++ lua/packages.lua
@@ -1,15 +1,5 @@
 vim.cmd([[packadd packer.nvim]])
 require("packer").startup(function()
-  use {
-  "folke/which-key.nvim",
-  config = function()
-    require("which-key").setup {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
-    }
-  end
-  }
   use("wbthomason/packer.nvim")
   use {
     "williamboman/mason.nvim",
